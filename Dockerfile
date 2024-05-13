fb6f3f47fd63a7f8923c58e6c7e048bceae3d1b3
Will Fowlkes <williamowenfowlk@gmail.com>
2024-05-13 13:49:16 -0400

    Implement nginx support

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,12 @@ WORKDIR /code
 RUN apk add --no-cache gcc musl-dev linux-headers
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
+
+ENV HOME=/home/app
+ENV APP_HOME=/home/app/web
+RUN mkdir -p $APP_HOME
+RUN mkdir -p $APP_HOME/static
+COPY /static $APP_HOME/static
+
 EXPOSE 8000
 COPY . .
--- cardcollector/settings.py
+++ cardcollector/settings.py
@@ -16,6 +16,8 @@ import environ
 
 from datetime import timedelta
 
+import os
+
 env = environ.Env()
 environ.Env.read_env()
 
@@ -139,6 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
