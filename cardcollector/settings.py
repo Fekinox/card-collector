9b5d33934ce72e8caef51969ea760427e120c969
Will Fowlkes <williamowenfowlk@gmail.com>
2024-05-14 11:52:45 -0400

    Add extra read env line

--- cardcollector/settings.py
+++ cardcollector/settings.py
@@ -26,6 +26,7 @@ env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
